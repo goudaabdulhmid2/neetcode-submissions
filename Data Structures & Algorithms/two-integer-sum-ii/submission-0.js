72ce09c97f5c3b32e08294665755c08faaea7db5
Abdulhamid Gouda <108636877+goudaabdulhmid2@users.noreply.github.com>
2026-06-29 18:19:59 +0300

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        for (let idx = 0; idx < numbers.length; idx++) {
+            let num = numbers[idx];
+            let remind = target - num;
+            
+            let idx2 = numbers.findIndex((ele, idxA) => ele === remind && idxA !== idx);
+            
+            if (idx2 !== -1) {
+                return [idx + 1, idx2 + 1];
+            }
+        }
+    }
+}
