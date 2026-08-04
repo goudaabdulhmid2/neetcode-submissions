6152bc226d07967b9402a171c190ace7c83689df
Abdulhamid Gouda <108636877+goudaabdulhmid2@users.noreply.github.com>
2026-08-04 22:30:07 +0300

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.js
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        
+        // search on the array
+        let l = 0, r = matrix.length - 1, mid = Math.floor((l+r)/2), idx = -1
+
+
+        while(l<=r){
+            console.log("mid:" + mid)
+            if(target > matrix[mid][ matrix[mid].length - 1] ){
+                l = mid + 1;
+                mid =  Math.floor((l+r)/2)
+            }else if (target < matrix[mid][0]){
+                r = mid  - 1;
+                mid =  Math.floor((l+r)/2)
+            }else{
+                idx = mid
+                break;
+
+            }
+        }
+        console.log(idx)
+       if(idx == -1) return false
+       l = 0, r = matrix[idx].length-1, mid = Math.floor((l+r)/2)
+       while(l<=r){
+        if(matrix[idx][mid] > target){
+            r = mid - 1;
+            mid = Math.floor((l+r)/2)
+        }else if(matrix[idx][mid] < target){
+            l = mid + 1;
+            mid =  Math.floor((l+r)/2)
+        }else{
+            return true
+        }
+       }
+
+       return false
+        
+    }
+
+}
